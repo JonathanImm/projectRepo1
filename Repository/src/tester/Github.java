0fc98751c0b8f532942a2b72cb748f83f696c35d
USER <USER@DESKTOP-UK5VDPC>
2022-07-07 16:37:38 +0700

    Setelah files dimasukkan ke "Staged Changes", tekan "Commit and Push" untuk mengupload files

---
+++ Repository/src/tester/Github.java
@@ -0,0 +1,17 @@
+package tester;
+
+public class Github {
+	
+	
+	public static void main(String[] args) {
+	
+		int num = 20;
+		int res = 30 + num;
+		
+		System.out.println(res);
+		
+	}
+	
+	
+	
+}
